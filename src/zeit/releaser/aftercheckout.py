b4a68ecf76c88b9c2c6e239382196329d3c9886e
Ron Drongowski <ron.drongowski@gmail.com>
2014-02-23 18:30:33 +0100

    Use better destination.

--- src/zeit/releaser/aftercheckout.py
+++ src/zeit/releaser/aftercheckout.py
@@ -17,7 +17,7 @@ def copy_unstaged_sources(data):
 
 def copy_js_css(src, dest):
     print 'copying %s/%s/css to %s/%s' % (src, target, dest, target)
-    shutil.copytree('%s/%s/css' % (src, target), '%s/%s' % (dest, target))
+    shutil.copytree('%s/%s/css' % (src, target), '%s/%s/css' % (dest, target))
 
     print 'copying %s/%s/js to %s/%s' % (src, target, dest, target)
-    shutil.copytree('%s/%s/js' % (src, target), '%s/%s' % (dest, target))
+    shutil.copytree('%s/%s/js' % (src, target), '%s/%s/js' % (dest, target))
