a5323e31fc1eb64ff47e4e8fd6ec9944f5284a90
Ron Drongowski <ron.drongowski@gmail.com>
2014-02-23 15:47:24 +0100

    Add first tests

--- src/zeit/releaser/aftercheckout.py
+++ src/zeit/releaser/aftercheckout.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def copy_js_css(data):
-    print "FOOOOO"
+import zest.releaser.utils
+import shutil
+
+target = 'src/zeit/frontend'
+
+
+def copy_unstaged_sources(data):
+    if not data['name'] == 'zeit.frontend':
+        return
+    if not zest.releaser.utils.ask('Copy js and css to tag-checkout'):
+        return
+    copy_js_css(data['workingdir'], data['tagdir'])
+
+
+def copy_js_css(src, dest):
+    shutil.copytree('%s/%s/css' % (src, target), '%s/%s' % (src, target))
+    shutil.copytree('%s/%s/js' % (src, target), '%s/%s' % (src, target))
---
+++ src/zeit/releaser/test/test_aftercheckout.py
@@ -0,0 +1,32 @@
+import zeit.releaser.aftercheckout
+import zest.releaser.utils
+from mock import Mock
+from mock import patch
+
+def test_copy_unstaged_src_should_be_processed_for_frontend(monkeypatch):
+    def ask(q):
+        return True
+    monkeypatch.setattr(zest.releaser.utils, 'ask', ask)
+
+    copy_js_css = Mock(return_value='True')
+    monkeypatch.setattr(
+        zeit.releaser.aftercheckout, 'copy_js_css', copy_js_css)
+
+    data = {}
+    data['name'] = 'zeit.frontend'
+    data['workingdir'] = 'workingdir'
+    data['tagdir'] = 'tagdir'
+
+    with patch.object(zeit.releaser.aftercheckout, 'copy_js_css') as mock_method:
+        mock_method.return_value = None
+        zeit.releaser.aftercheckout.copy_unstaged_sources(data)
+
+    mock_method.assert_called_once_with('workingdir', 'tagdir')
+
+def test_copy_unstaged_src_should_only_be_processed_for_frontend(monkeypatch):
+    def ask(q):
+        return True
+    data = {}
+    data['name'] = 'something'
+    monkeypatch.setattr(zest.releaser.utils, 'ask', ask)
+    assert zeit.releaser.aftercheckout.copy_unstaged_sources(data) is None
