dd9cef43d0c0f5024babcef535a420075bf360f0
Ron Drongowski <ron.drongowski@gmail.com>
2014-02-23 11:15:41 +0100

    Setup basic structure.

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(
+    name='zeit.releaser',
+    version='0.1.dev0',
+    long_description = long_description,
+    author=u'Ron Drongowski',
+    author_email='ron.drongowski@zeit.de',
+    install_requires=[
+          'setuptools',
+          'zest.releaser>=3.12',
+          # -*- Extra requirements: -*-
+      ],
+    entry_points={
+        'zest.releaser.releaser.after_checkout': [
+                'action = zeit.releaser.aftercheckout:copy_js_css',
+        ],
+    },
+    extras_require={
+       'test': [
+            'mock',
+            'pytest',
+        ],
+    },
+    namespace_packages=['zeit', 'zeit.releaser'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+)
