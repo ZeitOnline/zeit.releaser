368fe98cc2f1f020ce40aa8ed21e0c3e3bc12110
Ron Drongowski <ron.drongowski@gmail.com>
2014-09-18 17:58:48 +0200

    Delete subtree before copying

--- src/zeit/releaser/aftercheckout.py
+++ src/zeit/releaser/aftercheckout.py
@@ -16,5 +16,7 @@ def copy_unstaged_sources(data):
 
 
 def copy_js_css(src, dest):
+    print 'deleting %s/%s' % (dest, target)
+    shutil.rmtree('%s/%s' % (dest, target))
     print 'copying %s/%s to %s/%s' % (src, target, dest, target)
     shutil.copytree('%s/%s' % (src, target), '%s/%s' % (dest, target))
