1f00eaf5937dc13d98b90e6a9710c5a413f0c707
Ron Drongowski <ron.drongowski@gmail.com>
2014-09-18 17:59:00 +0200

    Preparing release 0.3.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (
 
 setup(
     name='zeit.releaser',
-    version='0.4.dev0',
+    version='0.3.2',
     long_description = long_description,
     author=u'Ron Drongowski',
     author_email='ron.drongowski@zeit.de',
